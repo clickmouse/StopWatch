45366474eb0d586ee03506ab76761e6c9b698e6f
clickmouse <17342015683@163.com>
2019-02-28 12:37:43 +0800

    StopWatch

---
+++ app/src/main/java/com/example/stopwatch2/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.stopwatch2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text1, text2;
+    Button button;
+    ImageView imageView;
+    Animation animation,btngone,btntwo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text1 = findViewById(R.id.text_view);
+        text2 = findViewById(R.id.text_view2);
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.background);
+        animation = AnimationUtils.loadAnimation(this, R.anim.animation);
+        btngone = AnimationUtils.loadAnimation(this,R.anim.btngone);
+        btntwo = AnimationUtils.loadAnimation(this,R.anim.btntwo);
+        imageView.startAnimation(animation);
+        text1.startAnimation(btngone);
+        text1.startAnimation(btngone);
+        button.startAnimation(btntwo);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent view = new Intent(MainActivity.this, StopWatch.class);
+                view.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(view);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/stopwatch2/StopWatch.java
@@ -0,0 +1,46 @@
+package com.example.stopwatch2;
+
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+
+public class StopWatch extends AppCompatActivity {
+
+    Button btnstart,btnstop;
+    ImageView pointer;
+    Animation round;
+    Chronometer time;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stop_watch);
+        btnstart = findViewById(R.id.btnstart);
+        pointer = findViewById(R.id.pointer);
+        btnstop = findViewById(R.id.btnstop);
+        time = findViewById(R.id.time);
+        round = AnimationUtils.loadAnimation(this,R.anim.round);
+        btnstop.setAlpha(0);
+        btnstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pointer.startAnimation(round);
+                btnstop.animate().alpha(1).translationY(-80).setDuration(300).start();
+                btnstart.animate().alpha(0).setDuration(300).start();
+                time.setBase(SystemClock.elapsedRealtime());
+                time.start();
+            }
+        });
+        btnstop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
